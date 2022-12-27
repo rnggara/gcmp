22a6d455d7bf2b735f7679494c42901b8fefd587
Rangga <ranggaangara8@gmail.com>
2022-12-27 08:36:32 +0700

    upload a file

--- request.php
+++ request.php
@@ -5,21 +5,44 @@ $action = filter_var(trim($_REQUEST['action']), FILTER_SANITIZE_STRING);
 $bucketName = "website-static";
 $cloudPath = "uploads/";
 if ($action == 'upload') {
-    $v = $_GET['v'];
+    // $v = $_GET['v'];
+    // $response['code'] = "200";
+
+    // $cloudPath = "uploads/v$v.txt";
+
+    // $fileContent = file_get_contents("v$v.txt");
+
+    // $isSucceed = uploadFile($bucketName, $fileContent, $cloudPath);
+    // if ($isSucceed == true) {
+    //     $response['msg'] = 'SUCCESS: to upload ' . $cloudPath . PHP_EOL;
+    //     // TEST: get object detail (filesize, contentType, updated [date], etc.)
+    //     $response['data'] = getFileInfo($bucketName, $cloudPath);
+    // } else {
+    //     $response['code'] = "201";
+    //     $response['msg'] = 'FAILED: to upload ' . $cloudPath . PHP_EOL;
+    // }
+    // header("Content-Type:application/json");
+    // echo json_encode($response);
+    // exit();
     $response['code'] = "200";
-
-    $cloudPath = "uploads/v$v.txt";
-
-    $fileContent = file_get_contents("v$v.txt");
-
-    $isSucceed = uploadFile($bucketName, $fileContent, $cloudPath);
-    if ($isSucceed == true) {
-        $response['msg'] = 'SUCCESS: to upload ' . $cloudPath . PHP_EOL;
-        // TEST: get object detail (filesize, contentType, updated [date], etc.)
-        $response['data'] = getFileInfo($bucketName, $cloudPath);
-    } else {
-        $response['code'] = "201";
-        $response['msg'] = 'FAILED: to upload ' . $cloudPath . PHP_EOL;
+    if ($_FILES['file']['error'] != 4) {
+        //set which bucket to work in
+        $bucketName = "cloud-test-bucket-1";
+        // get local file for upload testing
+        $fileContent = file_get_contents($_FILES["file"]["tmp_name"]);
+        // NOTE: if 'folder' or 'tree' is not exist then it will be automatically created !
+        $cloudPath = 'uploads/' . $_FILES["file"]["name"];
+ 
+        $isSucceed = uploadFile($bucketName, $fileContent, $cloudPath);
+ 
+        if ($isSucceed == true) {
+            $response['msg'] = 'SUCCESS: to upload ' . $cloudPath . PHP_EOL;
+            // TEST: get object detail (filesize, contentType, updated [date], etc.)
+            $response['data'] = getFileInfo($bucketName, $cloudPath);
+        } else {
+            $response['code'] = "201";
+            $response['msg'] = 'FAILED: to upload ' . $cloudPath . PHP_EOL;
+        }
     }
     header("Content-Type:application/json");
     echo json_encode($response);
--- upload.php
+++ upload.php
@@ -1,31 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div id="msg"></div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-    <script>
-        var s = 1;
-        var interval = setInterval(() => {
-            if(s > 5){
-                clearInterval(interval)
-            } else {
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>GCP Storage File Upload using PHP</title>
+    </head>
+    <body>
+        <form id="fileUploadForm" method="post" enctype="multipart/form-data">
+            <input type="file" name="file"/>
+            <input type="submit" name="upload" value="Upload"/>
+            <span id="uploadingmsg"></span>
+            <hr/>
+            <strong>Response (JSON)</strong>
+            <pre id="json">json response will be shown here</pre>
+            
+            <hr/>
+            <strong>Public Link</strong> <span>(https://storage.googleapis.com/[BUCKET_NAME]/[OBJECT_NAME])</span><br/>
+            <b>Note:</b> we can use this link only if object or the whole bucket has made public, which in our case has already made bucket public<br/>
+            <div id="output"></div>
+        </form>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <script>
+            $("#fileUploadForm").submit(function (e) {
+                e.preventDefault();
+                var action = "requests.php?action=upload";
+                $("#uploadingmsg").html("Uploading...");
+                var data = new FormData(e.target);
                 $.ajax({
-                    url : "request.php?action=upload&v=" + s,
-                    type : "get",
-                    dataType : "json",
-                    success : function(resp){
-                        $("#msg").append(`File v${s}.txt : ${resp.msg} <br>`)
+                    type: 'POST',
+                    url: action,
+                    data: data, 
+                    /*THIS MUST BE DONE FOR FILE UPLOADING*/
+                    contentType: false,
+                    processData: false,
+                }).done(function (response) {
+                    $("#uploadingmsg").html("");
+                    $("#json").html(JSON.stringify(response, null, 4));
+                    //https://storage.googleapis.com/[BUCKET_NAME]/[OBJECT_NAME]
+                    $("#output").html('<a href="https://storage.googleapis.com/' + response.data.bucket + '/' + response.data.name + '"><i>https://storage.googleapis.com/' + response.data.bucket + '/' + response.data.name + '</i></a>');
+                    if(response.data.contentType === 'image/jpeg' || response.data.contentType === 'image/jpg' || response.data.contentType === 'image/png') {
+                        $("#output").append('<br/><img src="https://storage.googleapis.com/' + response.data.bucket + '/' + response.data.name + '"/>');
                     }
-                })
-                s++
-            }
-        }, 1000);
-    </script>
-</body>
+                }).fail(function (data) {
+                    //any message
+                });
+            });  
+        </script>
+    </body>
 </html>
